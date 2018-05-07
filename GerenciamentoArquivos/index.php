8e88fc3a624d31768781823578f2a8bc993b8b5e
Ilana Quintino da Silva <ilana_silva@hotmail.com>
2018-05-07 14:26:15 -0300

    More archived projects

---
+++ GerenciamentoArquivos/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+   "http://www.w3.org/TR/html4/loose.dtd">
+
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>Sistema de Gerenciamento de Arquivos</title>
+</head>
+<body>
+<h1>Acesso</h1>
+<hr>
+<div align="center" style="width:300px;border-top:1px solid #0091F0;border-bottom:1px solid #0091F0;border-right:1px solid #0091F0;border-left:1px solid #0091F0;">
+    <br>
+    <form name="login" method="POST" action="projetos_andamento.php">
+        <br>
+    <table>
+        <tr><td>Usuário: </td><td><input type="text" name="usuario"></td></tr>
+        <tr><td>Senha: </td><td><input type="password" name="senha"></td></tr>
+    </table>
+    <br>
+    <input type="submit" value="Entrar"><br><br>
+</form>
+</div>
+</body>
+</html>
